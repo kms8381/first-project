e9a8df07f34f2ba9762ae7507abeb657f20e1bcd
kimminseok <kms3288381@naver.com>
2022-09-17 20:13:54 +0900

    first commit

--- project.py
+++ project.py
@@ -17,13 +17,17 @@ def web_project_post():
     chair_receive = request.form['chair_give']
     chair2_receive = request.form['chair2_give']
     star_receive = request.form['star_give']
+    commend_list = list(db.project.find({},{'_id':False}))
+    count = len(commend_list) + 1
 
-    doc = {'title':title_receive,
-           'commend':commend_receive,
-           'place':place_receive,
-           'chair':chair_receive,
-           'chair2':chair2_receive,
-           'star':star_receive
+    doc = {
+            'num': count,
+            'title':title_receive,
+            'commend':commend_receive,
+            'place':place_receive,
+            'chair':chair_receive,
+            'chair2':chair2_receive,
+            'star':star_receive
            }
     db.project.insert_one(doc)
     return jsonify({'msg': '입력 완료!'})
@@ -33,6 +37,12 @@ def web_project_post():
 def web_project_get():
     order_list = list(db.project.find({}, {'_id': False}))
     return jsonify({'orders': order_list})
+
+@app.route("/project/delete", methods=["POST"])
+def web_project_delete():
+    num_receive = request.form['num_give']
+    db.project.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
